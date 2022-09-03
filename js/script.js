9d821baa039a1d7b881af42dcbd5f05c65376945
Programmer-Habib <111034610+Programmer-Habib@users.noreply.github.com>
2022-09-03 07:49:44 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Slider area
+$(document).ready(function(){
+    $('.slider').bxSlider();
+});
+
+// mobile menu
+function openNav() {
+    document.getElementById("mysidenav").style.width="40%";
+}
+function closeNav() {
+    document.getElementById("mysidenav").style.width="0%";
+}
